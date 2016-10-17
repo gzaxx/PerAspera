365b676e0399d3e47bed09b440d8772b83f66732
gzaxx <gzaxxor@gmail.com>
2016-10-17 22:08:45 +0200

    Navigation menu added

---
+++ app/components/TopMenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ui stackable menu">
+        <div class="ui container">
+            <a class="item">
+                <i class="home icon"></i> Start
+            </a>
+            <a class="item">
+                <i class="calendar icon"></i> Historia
+            </a>
+            <a class="item">
+                <i class="settings icon"></i> Opcje
+            </a>               
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
