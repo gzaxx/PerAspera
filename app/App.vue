32c313ce2131e68d986ad4c4641452fba8dd8897
gzaxx <gzaxxor@gmail.com>
2016-10-17 22:09:34 +0200

    Changed entry view show splash screen if config exists show current month else show config

--- app/App.vue
+++ app/App.vue
@@ -1,15 +1,58 @@
 <template>
     <div id="app">
-        <config></config>
+        <component :is="currentView" transition="fade" transition-mode="out-in"></component>
     </div>
 </template>
 
 <script>
     import Config from './configuration/Config.vue'
+    import TopMenu from './components/TopMenu.vue'
+    import Current from './components/Current.vue'    
+    import Welcome from './components/Welcome.vue'        
+    import config from './configuration/config.js'
+    import bus from './core/eventBus.js'
 
     export default {
         components: {
+            Welcome, 
+            Current,
             Config
+        },
+        data() {
+            return {
+                currentView: Welcome,
+                views: {
+                    'Welcome': Welcome, 
+                    'Current': Current,
+                    'Config': Config
+                }
+            }
+        },
+        mounted() {
+            bus.emit('progress-set')
+
+            const self = this
+
+            setTimeout(function() {                
+                bus.emit('progress-set', 'ALMOST DONE')
+
+                setTimeout(function() {
+                    if (config.configurationExists()) {
+                        self.currentView = Current
+                    } else {
+                        self.currentView = Config
+                }}, 400)
+            }, 1000)
+
+            bus.on('view-change', this.viewChange)
+        },
+        methods: {
+            viewChange(name) {
+                var view = this.views[name]
+                if (view) {
+                    this.currentView = view
+                }
+            }
         }
     }
 </script>
